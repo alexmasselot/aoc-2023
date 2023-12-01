f66c3c65aa415e207e636c8e3e0437ad02c92209
Alex Masselot <alexandre.masselot@loccitane.com>
2023-12-01 10:32:01 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,67 @@
+fun extractDigits(input: String): List<Int> {
+    val wordToDigit = mapOf(
+        "zero" to "0",
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9",
+        "0" to "0",
+        "1" to "1",
+        "2" to "2",
+        "3" to "3",
+        "4" to "4",
+        "5" to "5",
+        "6" to "6",
+        "7" to "7",
+        "8" to "8",
+        "9" to "9"
+    )
+
+    val regex = Regex("(" + wordToDigit.keys.joinToString("|") + ")")
+    return wordToDigit.entries.flatMap { (text, v) ->
+        Regex(text).findAll(input).map { it.range.first to v }
+    }
+        .sortedBy { it.first }
+        .map { it.second.toInt() }
+        .toList()
+
+}
+
 fun main() {
+    // Locate digit in a string and return the first and the last ones concatenated
+
+    fun locateDigit(input: String): String {
+        val digits = input.filter { it.isDigit() }
+        return "${digits.first()}${digits.last()}"
+    }
+
+    // locate digit written in words in a string and return the first and the last ones concatenated
+    fun locateDigitInWords(input: String): String {
+        val digits = extractDigits(input)
+        return if (digits.isNotEmpty()) "${digits.first()}${digits.last()}" else ""
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { locateDigit(it).toInt() }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        return input.map { locateDigitInWords(it).toInt() }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day01/part1_test")
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("day01/part2_test")
+    check(part2(testInput2) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("day01/part1")
     part1(input).println()
     part2(input).println()
 }
---
+++ src/Day01KtTest.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day01KtTest {
+    @Test
+    fun `test xa1dfs3sdf5`() {
+
+        val got = extractDigits("xa1dfs3sdf5")
+        val expected = listOf(1, 3, 5)
+
+        assertEquals(expected, got)
+    }
+
+    @Test
+    fun `test sevenxxxeighthreetwo`() {
+
+        val got = extractDigits("sevenxxxeighthreetwo")
+        val expected = listOf(7, 8, 3, 2)
+
+        assertEquals(expected, got)
+    }
+    @Test
+    fun `test sevenxxxeightthreetwo`() {
+
+        val got = extractDigits("sevenxxxeightthreetwo")
+        val expected = listOf(7, 8, 3, 2)
+
+        assertEquals(expected, got)
+    }
+}
